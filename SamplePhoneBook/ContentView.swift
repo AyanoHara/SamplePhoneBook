d55f5b793506459c95fc9aa2db9edd7815addd3b
Ayano Hara <ayano0127@gmail.com>
2021-01-21 20:50:44 +0900

    Viewを別ファイルに分ける

--- SamplePhoneBook/ContentView.swift
+++ SamplePhoneBook/ContentView.swift
@@ -23,31 +23,18 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            NavigationView {
-                ZStack {
-                    Color(#colorLiteral(red: 0.5974730564, green: 0.799616446, blue: 0.9255421162, alpha: 1))
-                }
-                .navigationBarItems(leading: Image(systemName: "list.dash"))
-                .navigationBarTitle("プロフィール一覧", displayMode: .inline)
-            }
-            .tabItem {
-                Image(systemName: "list.dash")
-                    .foregroundColor(.black)
-                Text("プロフィール一覧")
-            }.tag(1)
-            
-            NavigationView {
-                ZStack {
-                    Color(#colorLiteral(red: 0.5974730564, green: 0.799616446, blue: 0.9255421162, alpha: 1))
-                }
-                .navigationBarItems(leading: Image(systemName: "person.circle.fill"))
-                .navigationBarTitle("プロフィール登録", displayMode: .inline)
-            }
-            .tabItem {
-                Image(systemName: "person.circle.fill")
-                    .foregroundColor(.black)
-                Text("プロフィール登録")
-            }.tag(2)
+            ProfileListView()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                        .foregroundColor(.black)
+                    Text("プロフィール一覧")
+                }.tag(1)
+            CreateProfileView()
+                .tabItem {
+                    Image(systemName: "person.circle.fill")
+                        .foregroundColor(.black)
+                    Text("プロフィール登録")
+                }.tag(2)
         }
         // 選択したアイテム色を指定
         .accentColor(.black)
